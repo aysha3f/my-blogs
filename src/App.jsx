b955bc6ed5607b1db6c67c9d92f25eb498174320
aysha3f <tahiaysha220@gmail.com>
2025-10-21 21:02:47 +0600

    Create Fake Data for the blogs and load data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
+import Blogs from './components/Blogs/Blogs'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
 <Navbar></Navbar>
+<Blogs></Blogs>
     </>
   )
 }
