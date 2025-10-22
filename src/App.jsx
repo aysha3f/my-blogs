e57f66bf85fc57c0efb5e9970a0482a4bdac23e5
aysha3f <tahiaysha220@gmail.com>
2025-10-22 20:51:22 +0600

    Set Layout and make react responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import Blogs from './components/Blogs/Blogs'
@@ -9,7 +8,21 @@ function App() {
   return (
     <>
 <Navbar></Navbar>
-<Blogs></Blogs>
+
+
+<div className="main-container flex text-center">
+  <div className="left-container w-[70%]">
+   
+
+   <Blogs></Blogs>
+  </div>
+  <div className="right-container w-[30%]">
+ <h1>Reading time: 0</h1>
+ <h1>bookmakred count: 0</h1>
+  </div>
+</div>
+
+
     </>
   )
 }
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -10,10 +10,10 @@ const Blogs = () => {
     .then(data => setBlogs(data))
    },[])
 
-//    console.log(blogs)
+   console.log(blogs)
     return (
         <div>
-            
+          <h1 className='text-3xl'>total: {blogs.length}</h1>  
         </div>
     );
 };
