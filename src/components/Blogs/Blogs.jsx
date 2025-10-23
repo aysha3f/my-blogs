b4bcbf2e555f82d9c8f0a84c32e53f9dc372a0e8
aysha3f <tahiaysha220@gmail.com>
2025-10-23 12:17:05 +0600

    Display Blog data and hashtags

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,4 +1,5 @@
 import React, {  useEffect, useState } from 'react';
+import Blog from '../Blog/Blog';
 
 const Blogs = () => {
    const [blogs, setBlogs] = useState([])
@@ -14,6 +15,12 @@ const Blogs = () => {
     return (
         <div>
           <h1 className='text-3xl'>total: {blogs.length}</h1>  
+
+          <div className="all-blogs grid grid-cols-2">
+            {
+              blogs.map((blog)=><Blog blog={blog}></Blog>)
+            }
+          </div>
         </div>
     );
 };
